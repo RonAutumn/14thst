6d082ed3d912c9d5e952ffbd4224f1d77a8466c4
Your Name <you@example.com>
2025-01-25 06:12:07 -0500

    Simplify PostCSS config to use Next.js defaults

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss/nesting': {},
-    'tailwindcss': require('tailwindcss'),
-    'autoprefixer': require('autoprefixer'),
-    'postcss-flexbugs-fixes': require('postcss-flexbugs-fixes'),
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3,
-      features: {
-        'custom-properties': false
-      }
-    }
-  }
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
